6b0cd5e6a7669cb25d05a37805af2219399bf3f1
B0REDC00K1E <marijacrv7@gmail.com>
2022-01-09 17:27:25 +0100

    Final commit

--- 31AverageHeight.cpp
+++ 31AverageHeight.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
 using namespace std;
 
+int print(double *a, int n)   //za pecatenje na niza
+{
+    for(int i=0; i<n; i++)
+    {
+        cout<<a[i]<< "  ";
+    }
+    cout<<endl;
+}
+
 int main()    //Inki848
 {
     int n,x;
-    int cm[n];
-    
     cout<<"Enter number of club members: ";
     cin>>n;
     cout<<endl;
     
+    double cm[n];
     cout<<"Enter each members' height in cm!"<<endl<<"(Note:Must be between 150-200cm)"<<endl;  
     for(int i=0; i<n; i++) 
     {
         cin>>x;
-        if(x>=150 && x<=200)
+        if(x>150 && x<=200)
         {
             cm[i]=x;
         } else 
@@ -28,11 +36,9 @@ int main()    //Inki848
     cout<<endl;
     
     cout<<"Each member's height is: "<<endl;   //Pecati nizata...
-    for(int i=0; i<n; i++)
-    {
-        cout<<cm[i]<< "  ";
-    }
+    print(cm,n);
     cout<<endl;
+    
     
     double sum=0, av=0;     //prosecna visina...
     for(int i=0; i<n; i++) 
@@ -40,13 +46,38 @@ int main()    //Inki848
         sum=cm[i]+sum;
     }
     av=sum/n;
-    cout<<"Average height of the members is: "<<av<<endl;
+    cout<<"Average height of the members is: "<<av<<endl<<endl;
     
     double avl=av-10, avh=av+10;      //visini koi variraat za +/-10 od prosecnata visina
     cout<<"Average + 10: "<<avh<<endl;
     cout<<"Average - 10: "<<avl<<endl;
+    cout<<"Heights that vary by +/- 10 centimeters of the average height: ";
+    for(int i=0;i<n;i++)
+ 	  if(cm[i]>avl && cm[i]>avh)
+	    cout<<cm[i]<<" ";
+	cout<<endl<<endl;
     
     double min, max;
-    
+    min = cm[0];   
+    max = cm[0];   
+    for(int i=0; i<n; i++)
+    {
+        if(cm[i]<=min)  //find minimal number
+        {
+            min = cm[i];
+        }
+        if(cm[i]>=max)  //find maximal number
+        {
+            max = cm[i];
+        }
+    }
+    cout  << "Maximum height: "<<max<<endl;
+    cout  << "Minimum height: "<<min<<endl;
+
+    cout << "Heights that vary from 0 to + 15% to the minimum height, and 0 to -20% to the maximum height: "<<endl;
+    for(int i=0;i<n;i++)
+ 	  if(((cm[i]-min)/min)*100<=15 && ((cm[i]-max)/max)*100>=-20)
+	    cout<<cm[i]<<" ";     //visini koi variraat od 0 do +15% vo odnos na min visina i od 0 do -20% vo odnos na max visina
+	    
     return 0;
 }
